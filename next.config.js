02459206fc059e8ce211c9e41e4b3f933755b388
lebbs <35425109+lebbs@users.noreply.github.com>
2021-04-07 16:44:56 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     env: {
-        MONGO_URI: "mongodb+srv://testi123:Verohallinto69@cluster0.deg0q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        MONGO_URI: ""
     }
 }
